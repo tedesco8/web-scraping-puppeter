e569613e29763da4ad83dc6cbd960411cec01f05
tedesco8 <tedesco8@hotmail.com>
2021-01-10 18:02:40 -0300

    recuperando enlaces

--- comparador_api/domain/scraperMeli.js
+++ comparador_api/domain/scraperMeli.js
@@ -1,24 +1,33 @@
 const scraperObject = {
-    url: 'https://www.mercadolibre.com.uy/',
-    async scraper(browser, search){
-        let page = await browser.newPage();
-        let results = 0;
-        console.log(`Navigating to ${this.url}...`);
-        await page.goto(this.url);
-        await page.type(".nav-search-input", search);
-        await page.click(".nav-search-btn");
-        await page.waitForSelector(".ui-search-results");
-        const items = await page.evaluate(() => {
-            const elements = document.querySelectorAll('.ui-search-layout__item');
-            const objs = [];
-            for(let element in elements) {
-                objs.push({element});
-            }
-            return objs
-        });
-        results = items.length;
-        console.log(results)
-    }
-}
+  url: 'https://www.mercadolibre.com.uy/',
+  async scraper(browser, search) {
+    //Obtenemos la instancia de una nueva pagina
+    let page = await browser.newPage();
+    console.log(`Navigating to ${this.url}...`);
+
+    //Navegamos a la url
+    await page.goto(this.url);
+    //Escribimos en el selector nuestra busqueda
+    await page.type('.nav-search-input', search);
+    //Presionamos el boton de buscar
+    await page.click('.nav-search-btn');
+    //Espera la carga del selector
+    await page.waitForSelector('.ui-search-layout__item'); //Atributos entre corchetes '[ui-search-layout__item]'
+    //waitFor(300) esperamos tres segundos
+
+    //Capturamos enlaces
+    const enlaces = await page.evaluate(() => {
+
+      const nodes = document.getElementsByClassName("ui-search-result__image") 
+      const arrNodes=Array.from(nodes)
+      const links = arrNodes.map(n=n=>n.firstElementChild.href)
+      
+      return links;
+    });
+    console.log(enlaces.length)
+    console.log(enlaces);
+    //browser.close()
+  },
+};
 
 module.exports = scraperObject;
