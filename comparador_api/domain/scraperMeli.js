4ac0be56d8956b25db1ad896d072004f4b1a083f
Pablo Tedesco <tedesco8@hotmail.com>
2021-01-14 21:02:21 -0300

    obteniendo precios de articulos

--- comparador_api/domain/scraperMeli.js
+++ comparador_api/domain/scraperMeli.js
@@ -1,6 +1,7 @@
 const scraperObject = {
   url: 'https://www.mercadolibre.com.uy/',
   async scraper(browser, search) {
+    console.log("LLEGO")
     //Obtenemos la instancia de una nueva pagina
     let page = await browser.newPage();
     console.log(`Navigating to ${this.url}...`);
@@ -22,16 +23,25 @@ const scraperObject = {
       const links = arrNodes.map(n=n=>n.firstElementChild.href)
       return links;
     });
+    //Nos movemos a cada articulo y capturamos los datos
     const articles = []
     for(let enlace of enlaces) {
+      let i = 0
+      if (i === 3) { break; }
       await page.goto(enlace)
-      await page.waitForSelector('.ui-pdp-title')
+      await page.waitForSelector('.ui-pdp-container__col')
       const article = await page.evaluate(() => {
         const tmp = {}
         tmp.title = document.querySelector('.ui-pdp-title').innerText
+        let str = document.querySelector('.ui-pdp-price').innerText
+        // let newStr = str.slice(0,1)
+        // let newStr2 = str.slice(4)
+        // str = `${newStr} ${newStr2}`
+        tmp.price = str
         return tmp
       })
       articles.push(article)
+      i = i + 1
     }
     console.log(articles)
     //browser.close()
